2d85f6adf2f78012e9c06a9016477a469d24aa5e
Mortal_Master <mortal@mortaldeMacBook-Pro.local>
2016-12-10 20:41:30 +0800

    快速修改控件的 frame

--- 快速修改 frame/UIView+BWFrame.h
+++ 快速修改 frame/UIView+BWFrame.h
@@ -1,8 +1,8 @@
 //
 //  UIView+BWFrame.h
-//  （城觅）
+//  Miss-Scarlett
 //
-//  Created by mortal on 16/9/20.
+//  Created by mortal on 16/10/16.
 //  Copyright © 2016年 mortal. All rights reserved.
 //
 
@@ -10,14 +10,12 @@
 
 @interface UIView (BWFrame)
 
-/**
- 1.一般情况下，分类只能添加方法
- 2.如果想要添加属性，必须重写该属性的set与get方法
- 3.使用@property不会生成带有下划线的成员属性，会声明set与get方法
- */
-@property (nonatomic, assign) CGFloat x;
-@property (nonatomic, assign) CGFloat y;
-@property (nonatomic, assign) CGFloat width;
-@property (nonatomic, assign) CGFloat height;
+@property CGFloat bw_x;
+@property CGFloat bw_y;
+@property CGFloat bw_width;
+@property CGFloat bw_height;
+
+@property CGFloat bw_centerX;
+@property CGFloat bw_centerY;
 
 @end
--- 快速修改 frame/UIView+BWFrame.m
+++ 快速修改 frame/UIView+BWFrame.m
@@ -1,8 +1,8 @@
 //
 //  UIView+BWFrame.m
-//  （城觅）
+//  Miss-Scarlett
 //
-//  Created by mortal on 16/9/20.
+//  Created by mortal on 16/10/16.
 //  Copyright © 2016年 mortal. All rights reserved.
 //
 
@@ -10,48 +10,64 @@
 
 @implementation UIView (BWFrame)
 
-- (void)setX:(CGFloat)x {
+- (void)setBw_x:(CGFloat)bw_x {
     CGRect frame = self.frame;
-    frame.origin.x = x;
+    frame.origin.x = bw_x;
     self.frame = frame;
 }
 
-- (CGFloat)x {
+- (CGFloat)bw_x {
     return self.frame.origin.x;
 }
 
-
-- (void)setY:(CGFloat)y {
-    
+- (void)setBw_y:(CGFloat)bw_y {
     CGRect frame = self.frame;
-    frame.origin.y = y;
+    frame.origin.y = bw_y;
     self.frame = frame;
 }
 
-- (CGFloat)y {
+- (CGFloat)bw_y {
     return self.frame.origin.y;
 }
 
-
-- (void)setWidth:(CGFloat)width {
-    
+- (void)setBw_width:(CGFloat)bw_width {
     CGRect frame = self.frame;
-    frame.size.width = width;
+    frame.size.width = bw_width;
     self.frame = frame;
 }
 
-- (CGFloat)width {
+- (CGFloat)bw_width {
     return self.frame.size.width;
+    
 }
 
-
-- (void)setHeight:(CGFloat)height {
+- (void)setBw_height:(CGFloat)bw_height {
     CGRect frame = self.frame;
-    frame.size.height = height;
+    frame.size.height = bw_height;
     self.frame = frame;
 }
 
-- (CGFloat)height {
+- (CGFloat)bw_height {
     return self.frame.size.height;
+}
+
+- (void)setBw_centerX:(CGFloat)bw_centerX {
+    CGPoint center = self.center;
+    center.x = bw_centerX;
+    self.center = center;
+}
+
+- (CGFloat)bw_centerX {
+    return self.center.x;
+}
+
+- (void)setBw_centerY:(CGFloat)bw_centerY {
+    CGPoint center = self.center;
+    center.y = bw_centerY;
+    self.center = center;
+}
+
+- (CGFloat)bw_centerY {
+    return self.center.y;
 }
 @end
